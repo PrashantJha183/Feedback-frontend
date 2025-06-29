d1bc4f30845f73d355362bc8b53d70bdeb2a0c32
Prashant Jha <jhap5754@gmail.com>
2025-06-29 21:45:41 +0530

    Updated config file

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {
@@ -15,6 +16,5 @@ export default defineConfig({
         secure: false,
       },
     },
-    base: "/",
   },
 });
